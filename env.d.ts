dc96e5a2da06628409ce5201631c8fc89314648d
chendj001 <719857499@qq.com>
2024-02-11 18:21:34 +0800

    32

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,7 @@
 import "./auto-imports"
+declare module '*.md' {
+  import type { ComponentOptions } from 'vue'
+  const Component: ComponentOptions
+  export default Component
+}
 /// <reference types="vite/client" />
