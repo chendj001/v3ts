a23ed39e8109c5539e2ab7d228350f3e8367c9f4
chendj001 <719857499@qq.com>
2024-02-11 15:52:07 +0800

    文档描写

---
+++ src/md/ts/resolver.md
@@ -0,0 +1,7 @@
+### 编辑自己的 unplugin-auto-import/resolvers
+
+<<< @/src/resolver/index.ts
+
+同时导入`auto-imports.d.ts` 解决编辑器提示
+
+<<< @/env.d.ts{1}
--- src/resolver/index.ts
+++ src/resolver/index.ts
@@ -1,14 +1,18 @@
 import { join } from 'path'
 import { normalizePath } from 'vite'
-let hooks = ['sleep','useDialog']
-
+// 使用类型
 import type { Resolver } from 'unplugin-auto-import/types'
+/**
+ * 暴露哪些函数可以直接使用
+ */
+let hooks = ['sleep', 'useDialog']
+
 function resolveHooks(name: string) {
   if (!hooks) return
   if (!hooks.includes(name)) return
-
   return {
     name,
+    // 拼接路径并序列化
     from: normalizePath(join(process.cwd(), './src/utils/index.ts'))
   }
 }
